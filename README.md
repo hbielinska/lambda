d227b20a79820d345b6d054fa1fdd25b6fdad4a4
Hanna Bielinska <lirviria@gmail.com>
2023-12-20 13:23:08 +0100

    Dodanie pliku readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# lambda
+
+## csvreader.py
+Zawiera klasę CSvReader:
+odczytuje ona dane z pliku CSV, tworzy obiekty klasy Person na podstawie każdego wiersza w pliku CSV, a następnie zwraca listę tych obiektów. 
+
+## dane.csv
+Plik csv zawierający dane osobowe
+
+## dates.csv
+Plik csv w którym tworzony jest rok urodzin
+
+## main.py
+Plik główny:
+*tworzenie tabelki, dodawanie do niej danych z pliku csv
+*filtrowanie z użyciem lambdy
+*tworzenie pliku dates.csv (zawiera wygenerowane lata)
+*tworzenie pliku persons_data.csv - dane z pliku dane.csv + wygenerowane daty urodzenia
+
+## person.py
+Zawiera klasę Person:
+Struktura przechowywująca dane o osobie
+
+## persons_data.csv
+Zawiera dane z pliku dane.csv + wygenerowane daty urodzenia
+
+## sqlite3
+Baza danych zawierająca imie, nazwisko i adres osoby
--- main.py
+++ main.py
@@ -3,85 +3,94 @@ import os
 from person import Person
 import random
 from datetime import datetime, timedelta
+from csvreader import CsvReader
+import sqlite3
 
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect('sqlite3.db')
 
+    def createTable(self):
+        c = self.conn.cursor()
+        c.execute('''CREATE TABLE if not exists people
+                (first_name TEXT, last_name TEXT, address TEXT)''')
+        
+    def insert(self, person: Person):
+        c = self.conn.cursor()
+        query = f"""INSERT INTO people(first_name, last_name, address)
+            VALUES('{person.first_name}', '{person.last_name}', '{person.address}')"""
+        c.execute(query)
 
-class CsvReader:
-    @staticmethod
-    def ReadCsv(file_path):
-        persons = []
-
-        with open(file_path, 'r') as file:
-            reader = csv.reader(file)
-
-
-            for values in reader:
-                # print(values)
-                person = Person(list(map(str.strip, values)))
-                persons.append(person)
-
-
-        return persons
+    def saveAndCommit(self):
+        self.conn.commit()
+        
 
 if __name__ == "__main__":
     file_path = os.path.abspath("dane.csv")
     print(file_path)
 
-    all_people = CsvReader.ReadCsv(file_path)
+all_people = CsvReader.ReadCsv(file_path)
 
-    a_names = list(filter(lambda x: x.first_name.startswith('A'), all_people))
-    print(a_names)
+a_names = list(filter(lambda x: x.first_name.startswith('A'), all_people))
+print(a_names)
 
-    google_emails = list(filter(lambda x: '@gmail.com' in x.email, all_people))
-    print(google_emails)
-
-
-
-    print("\nImiona na 'A':")
-    person_with_name_starting_a = list(filter(lambda p: p.first_name.startswith('A') ,all_people))
-    print(person_with_name_starting_a)
-
-    print("\nImiona i nazwiska na ta sama litere")
-    same_letter = list(filter(lambda p: p.first_name[0] == p.last_name[0], all_people ))
-    print(same_letter)
-
-    print("\nMiasto o najwiekszej ilosci osob")
-    cities_list = list(map(lambda p: p.city, all_people))
-    mpopulous_city = max(cities_list, key=lambda p: cities_list.count(p))
-    print(mpopulous_city)
-
-    dof_birth = []
-
-    for i in range(501):
-        random_number = random.randint(18*365, 60*365)
-        current_date = datetime.now()
-        random_year = current_date - timedelta(days=random_number)
-        dof_birth.append(random_year.year)
-        #print(dof_birth[i])
+google_emails = list(filter(lambda x: '@gmail.com' in x.email, all_people))
+print(google_emails)
 
 
-    f_path = 'dates.csv'
-    with open (f_path, 'w', newline = '') as plik_csv:
-        writer = csv.writer(plik_csv)
 
-        for dataur in dof_birth:
-            writer.writerow([dataur])
+print("\nImiona na 'A':")
+person_with_name_starting_a = list(filter(lambda p: p.first_name.startswith('A') ,all_people))
+print(person_with_name_starting_a)
 
-    print(f'Dane zostały zapisane do pliku CSV: {f_path}')
+print("\nImiona i nazwiska na ta sama litere")
+same_letter = list(filter(lambda p: p.first_name[0] == p.last_name[0], all_people ))
+print(same_letter)
 
-    if __name__ == "__main__":
+print("\nMiasto o najwiekszej ilosci osob")
+cities_list = list(map(lambda p: p.city, all_people))
+mpopulous_city = max(cities_list, key=lambda p: cities_list.count(p))
+print(mpopulous_city)
+
+dof_birth = []
+
+for i in range(501):
+    random_number = random.randint(18*365, 60*365)
+    current_date = datetime.now()
+    random_year = current_date - timedelta(days=random_number)
+    dof_birth.append(random_year.year)
+    #print(dof_birth[i])
+
+
+f_path = 'dates.csv'
+with open (f_path, 'w', newline = '') as plik_csv:
+    writer = csv.writer(plik_csv)
+
+    for dataur in dof_birth:
+        writer.writerow([dataur])
+
+print(f'Dane zostały zapisane do pliku CSV: {f_path}')
+
+if __name__ == "__main__":
         file_path = os.path.abspath("dane.csv")
 
-        all_people = CsvReader.ReadCsv(file_path)
+all_people = CsvReader.ReadCsv(file_path)
+person_db = Database()
+person_db.createTable()
 
-        for p in all_people:
-            p.generate_birthday()
+for p in all_people:
+    person_db.insert(p)
 
-        f_path = 'persons_data.csv'
+person_db.saveAndCommit()
 
-        with open (f_path, 'w', newline = '') as plik_csv:
-            writer = csv.writer(plik_csv)
+for p in all_people:
+    p.generate_birthday()
 
-            for person in all_people:
-                writer.writerow(person.__dict__.values())
+f_path = 'persons_data.csv'
+
+with open (f_path, 'w', newline = '') as plik_csv:
+    writer = csv.writer(plik_csv)
+
+    for person in all_people:
+        writer.writerow(person.__dict__.values())
 
